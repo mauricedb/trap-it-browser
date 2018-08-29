3c31e9bbd0b68f507d4d3df2ff8fbf234fa796e2
Maurice de Beijer <maurice.de.beijer@gmail.com>
2018-08-29 13:46:18 +0200

    Start of implementation

---
+++ src/error-record.ts
@@ -0,0 +1,16 @@
+export class ErrorRecord {
+  public message: string;
+  public name: string;
+  public stack: string;
+  public when: string;
+  public secondsActive: number;
+
+  constructor(error: Error, secondsActive) {
+    this.message = error.message;
+    this.name = error.name;
+    this.stack = error.stack;
+    this.secondsActive = secondsActive;
+
+    this.when = new Date().toISOString();
+  }
+}
--- src/trap-it.ts
+++ src/trap-it.ts
@@ -1,7 +1,34 @@
-const windowErrorListener = (evt: ErrorEvent): void => {
-  console.log(evt);
+import { ErrorRecord } from './error-record';
+import { start } from 'repl';
+
+const startTime = Date.now();
+let errors: ErrorRecord[] = [];
+
+export const addError = (error: Error) => {
+  const secondsActive = (Date.now() - startTime) / 1000;
+  const errorRecord = new ErrorRecord(error, secondsActive);
+  errors.push(errorRecord);
 };
 
-export const trapGlobalErrors = () => {
+export const getAllErrors = () => {
+  return errors;
+};
+
+const windowErrorListener = (evt: ErrorEvent): void => {
+  console.error(`Unhandled promise rejection: ${evt.message}`);
+
+  addError(evt.error);
+};
+
+const unhandledrejectionListener = (evt: PromiseRejectionEvent): void => {
+  console.error(`Unhandled promise rejection: ${evt.reason}`);
+
+  const error = new Error(`Unhandled promise rejection: ${evt.reason}`);
+  addError(error);
+};
+
+export const trapGlobalErrors = (evt: ErrorEvent) => {
   window.addEventListener('error', windowErrorListener);
+  // Only supported by Chrome
+  window.addEventListener('unhandledrejection', unhandledrejectionListener);
 };
