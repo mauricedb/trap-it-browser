95a170745e5470c3cfa6232aceda409bddc6e067
Maurice de Beijer <maurice.de.beijer@gmail.com>
2018-09-01 09:23:33 +0200

    Catch unhandled promise rejections

--- src/trap-it.ts
+++ src/trap-it.ts
@@ -1,5 +1,4 @@
 import { ErrorRecord } from './error-record';
-import { start } from 'repl';
 
 const startTime = Date.now();
 let errors: ErrorRecord[] = [];
@@ -14,17 +13,49 @@ export const getAllErrors = () => {
   return errors;
 };
 
-const windowErrorListener = (evt: ErrorEvent): void => {
-  console.error(`Unhandled promise rejection: ${evt.message}`);
 
-  addError(evt.error);
+const windowErrorListener = (evt: ErrorEvent): void => {
+  try {
+    console.error(`Unhandled promise rejection: ${evt.message}`);
+
+    let error = evt.error || evt;
+
+    if (!(error instanceof Error)) {
+      try {
+        error = new Error(JSON.stringify(error));
+      } catch (ex) {
+        error = new Error(error);
+      }
+    }
+
+    addError(error);
+
+    evt.preventDefault();
+  } catch (err) {
+    console.error(`Trap-it: ${err}`);
+  }
 };
 
 const unhandledrejectionListener = (evt: PromiseRejectionEvent): void => {
-  console.error(`Unhandled promise rejection: ${evt.reason}`);
+  try {
+    console.log(
+      'evt instanceof PromiseRejectionEvent',
+      evt instanceof PromiseRejectionEvent
+    );
+    console.log('evt.reason = Error', evt.reason instanceof Error);
+    console.log('evt.reason = Response', evt.reason instanceof Response);
 
-  const error = new Error(`Unhandled promise rejection: ${evt.reason}`);
-  addError(error);
+    console.error(`Unhandled promise rejection: ${evt.reason}`);
+
+    
+    const error = new Error(`Unhandled promise rejection: ${evt.reason}`);
+
+    addError(error);
+
+    evt.preventDefault();
+  } catch (err) {
+    console.error(`Trap-it: ${err}`);
+  }
 };
 
 export const trapGlobalErrors = (evt: ErrorEvent) => {
