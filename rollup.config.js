dd9a72bae7784ee76e780d0cf6d1512d7b9383e9
Maurice de Beijer <maurice.de.beijer@gmail.com>
2018-09-21 16:32:59 +0200

    Build with hash-code

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,6 @@
 import typescript from 'rollup-plugin-typescript2';
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
 
 export default {
   input: './src/index.ts',
@@ -10,6 +12,8 @@ export default {
   plugins: [
     typescript({
       typescript: require('typescript')
-    })
+    }),
+    commonjs(),
+    resolve()
   ]
 };
